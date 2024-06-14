bc4615d4c753132399d008bcdd9d29a8f7cf126c
WidSa <115068307+WidSa@users.noreply.github.com>
2024-06-14 22:09:48 +0700

    Add files via upload

---
+++ rock-paper-scissors/main.py
@@ -0,0 +1,66 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+import random
+comp_choice = random.randint(0,2)
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice == 0:
+    print(rock)
+elif user_choice == 1:
+    print(paper)
+elif user_choice == 2:
+    print(scissors)
+else:
+    print("Invalid choice")
+print("Computer chose:")
+if comp_choice == 0:
+    print(rock)
+elif comp_choice == 1:
+    print(paper)
+elif comp_choice == 2:
+    print(scissors)
+else:
+    print("Invalid choice")
+
+if user_choice == comp_choice:
+    print("It's a draw")
+elif user_choice == 0 and comp_choice == 1:
+    print("You lose")
+elif user_choice == 0 and comp_choice == 2:
+    print("You win")
+elif user_choice == 1 and comp_choice == 0:
+    print("You win")
+elif user_choice == 1 and comp_choice == 2:
+    print("You lose")
+elif user_choice == 2 and comp_choice == 0:
+    print("You lose")
+elif user_choice == 2 and comp_choice == 1:
+    print("You win")
+else:
+    print("You type invalid choice. You Lose")
